8843238fe8c1f2b36e18d6b25a0e398865e29daa
AaronFang123 <2018091621006@std.uestc.edu.cn>
2019-12-09 15:34:35 +0800

    教师类完善，支持了成绩输入（文件保存）和图形界面

--- src/scoreManagementSys/Teacher.java
+++ src/scoreManagementSys/Teacher.java
@@ -3,11 +3,6 @@ package scoreManagementSys;
 import java.io.*;
 import java.util.*;
 import javax.swing.*;
-
-import scoreManagementSys.Student.CourseSearch;
-import scoreManagementSys.Student.PersonalInfo;
-import scoreManagementSys.Student.StdMenu;
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -196,7 +191,6 @@ public class Teacher extends JFrame{
     String changePWConfirm() {
     	String result = null;
 		for(;;) {
-    		//TODO 修改密码
 
     		String newPassWord1 = (String)JOptionPane.showInputDialog("输入新的密码");
     		if (newPassWord1 == null ) {
@@ -238,7 +232,7 @@ public class Teacher extends JFrame{
   		    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //想要只关闭子窗口，方法如下：子窗口设置为setDefaultCloseOption(Jframe.DISPOSE_ON_CLOSE)     
   		    this.setVisible(true);
   		    		    
-  		    JLabel labWelcome = new JLabel("欢迎您，同学"+Teacher.this.loginTea.name);
+  		    JLabel labWelcome = new JLabel("欢迎您，老师"+Teacher.this.loginTea.name);
   		    labWelcome.setBounds(50, 2, 550, 45);
   		      
   		    JLabel labChoosefunc = new JLabel("选择一个功能");
@@ -259,17 +253,202 @@ public class Teacher extends JFrame{
 		    
 		    btntapInfo.addActionListener(new ActionListener() {
 		          public void actionPerformed(ActionEvent e) {
-		        //TODO
-		          //new PersonalInfo();
+		          new PersonalInfo();
 		          }
 		          } );
 		    
 		    btntapLoginCourse.addActionListener(new ActionListener() {
 		          public void actionPerformed(ActionEvent e) {
-		        //TODO
-		          //new CourseSearch();
+		          new GradeInput();
 		          }
 		          } );
   		}
   	}
+  	
+  	class PersonalInfo extends JFrame{
+		public PersonalInfo() {
+			this.setBounds(300, 100, 500, 400);//位置参数
+		    this.setTitle("老师"+ Teacher.this.loginTea.name);//title
+		    this.setLayout(null);//布局
+		    this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);  //想要只关闭子窗口，方法如下：子窗口设置为setDefaultCloseOption(Jframe.DISPOSE_ON_CLOSE)     
+		    this.setVisible(true); 
+		    
+		    JLabel labWelcome = new JLabel("老师"+ Teacher.this.loginTea.name+"个人信息如下：");
+		    labWelcome.setBounds(50, 5, 400, 50);
+		    
+		    JLabel labteaNum = new JLabel("职工编号："+Teacher.this.loginTea.staffNum);
+		    labteaNum.setBounds(50, 35, 400, 50);
+		    
+		    JLabel labName = new JLabel("姓名："+Teacher.this.loginTea.name);
+		    labName.setBounds(50, 65, 400, 50);
+		    
+		    JLabel labDepa = new JLabel("学院："+Teacher.this.loginTea.department);
+		    labDepa.setBounds(50, 95, 400, 50);
+		    
+		    JLabel labSch = new JLabel("系："+Teacher.this.loginTea.school);
+		    labSch.setBounds(50, 125, 400, 50);
+		    
+		    JLabel labTips = new JLabel("选择下列功能");
+		    labTips.setBounds(50, 215, 400, 50);
+		    
+		    JButton buttPW = new JButton("更改密码");
+		    buttPW.setBounds(50, 285, 200, 30);
+		    
+		    this.add(labWelcome);
+		    this.add(labteaNum);
+		    this.add(labName);
+		    this.add(labDepa);
+		    this.add(labSch);
+		    this.add(labTips);
+		    this.add(buttPW);
+		    
+		    buttPW.addActionListener(new ActionListener() {
+		          public void actionPerformed(ActionEvent e) {
+		        	String newPassWord = changePWConfirm();//输入两次，确认密码
+		        	if (newPassWord == null) {
+						return;
+					}
+            		Teacher.this.loginTea.password = newPassWord;//更新密码
+            		Teacher.this.allTeaList.set(Teacher.this.loginTeaIndex, Teacher.this.loginTea);
+            		
+                  try {
+                      BufferedWriter bw = new BufferedWriter(new FileWriter("./teacher.txt"));
+                      for (Teacher teacher : Teacher.this.allTeaList) { // 写入teacher.txt保存更改
+                    	  bw.write(String.format("%s\t\t%s\t\t%s\t\t%s\t\t%s\n", teacher.staffNum, teacher.name, teacher.school, teacher.department, teacher.password));
+                      }
+                      bw.close();
+                  } catch (IOException e1) {
+                      e1.printStackTrace();
+                  }
+		          }
+		          } );
+				}
+  			}
+  	//成绩登录
+  	class GradeInput{
+  		 String line;
+         String course_to_enter;
+         String grade;
+         int n = 0;
+         boolean grade_enter = Boolean.FALSE; // 判断是否查询到该学生的指定课程的布尔值
+         final String[] info = new String[600];
+         public GradeInput() {
+        	for(;;) {
+        		//接收课程编号输入
+        		String courseNum = (String) JOptionPane.showInputDialog("请输入您要录入成绩的课程编号");
+        		if(courseNum == null) {
+        			break;
+        		}
+        		if (courseNum =="") {
+        			JOptionPane.showMessageDialog(null, "请正确输入！");
+        			continue;
+				}
+        		this.course_to_enter = courseNum;
+        		break;
+        	} 
+        	
+        	int op = JOptionPane.showConfirmDialog(null
+        			, "警告！\n输入开始后请按照提示进行输入直至程序正常退出\n随意取消或关闭输入将会导致数据丢失\n是否继续"
+        			,"警告",JOptionPane.YES_NO_CANCEL_OPTION);
+            if (op != JOptionPane.YES_OPTION) {
+				return;
+            }
+            else {
+        	try {
+                final BufferedReader br = new BufferedReader(
+                new FileReader(String.format("./Grade/%s.txt", course_to_enter)));
+                while (true) {
+                    try 
+                    {
+                      if ((line = br.readLine()) == null)
+                          break;
+                    } 
+                    catch (final IOException e) 
+                    {
+                        e.printStackTrace();
+                        return;
+                    }
+                    final Scanner scan4 = new Scanner(line).useDelimiter("\\s+");
+                    for (int i = 0; i < 6; i++) {
+                        info[n + i] = scan4.next();
+                    }
+                    if (info[2].equals(course_to_enter)) { // 打印匹配到的成绩信息
+                        grade_enter = Boolean.TRUE;
+                        //System.out.println("学号: " + info[n]);
+                        //System.out.println("姓名: " + info[n + 1]);
+                        //System.out.println("课程编号: " + info[n + 2]);
+                        //System.out.println("课程名称: " + info[n + 3]);
+                        //System.out.println("教师: " + info[n + 4]);
+                        JOptionPane.showMessageDialog(null, "您将要登录的成绩的课程信息"+"\n"
+                        +"学号: " + info[n]+ "\n" 
+                        +"姓名: " + (String)info[n+1] +"\n"
+                        +"课程编号: " + info[n + 2]+"\n"
+                        +"课程名称: " + info[n + 3]+"\n"
+                        +"教师: " + info[n + 4]);
+                        
+                        //final Scanner scan6 = new Scanner(System.in);
+                        //System.out.println(String.format("Please enter %s's grade of %s :", info[1], course_to_enter));
+                        //grade = scan6.next();
+                        
+                    	for(;;) {
+                    		//接收具体学生成绩输入
+                    		String grade = (String) JOptionPane.showInputDialog(String.format("请输入您输入学生  %s 的  %s 成绩",  info[1], this.course_to_enter));
+                    		if(grade == null) {
+                    			break;
+                    		}
+                    		if (grade =="") {
+                    			JOptionPane.showMessageDialog(null, "请正确输入！");
+                    			continue;
+            				}
+                            info[n + 5] = grade;
+                            n += 6;
+                            break;
+                    	} 
+                    }
+                }
+
+                try {
+                    int num1 = 0, num2 = 0;
+                    final BufferedWriter bw = new BufferedWriter(
+                    new FileWriter(String.format("./Grade/%s.txt", course_to_enter)));
+                    while (num1 < n) {
+                        while (num2 < 5) {
+                            bw.write(info[num1] + "\t");
+                            num1++;
+                            num2++;
+                        }
+                        bw.write(info[num1] + "\n");
+                        num1++;
+                        num2 = 0;
+                    }
+                    bw.flush();
+                    bw.close();
+                } catch (final IOException io) {
+                    io.printStackTrace();
+                }
+            } catch (final FileNotFoundException notfound) {
+                final File dir = new File("./Grade");
+                if (!dir.exists()) {
+                    if (dir.mkdir())
+                        //System.out.println("Folder not found, have create a new folder");
+                    	JOptionPane.showMessageDialog(null, "成绩文件不存在，请联系管理员，已临时新建文件夹");
+                }
+                final File file = new File(String.format("./Grade/%s.txt", course_to_enter));
+                try {
+                    if (file.createNewFile())
+                        //System.out.println(String.format("%s.txt not found, have created new file.", course_to_enter));
+                    	JOptionPane.showMessageDialog(null, "该课程成绩文件不存在，请联系管理员，已临时文件");
+                } catch (final IOException io) {
+                    io.printStackTrace();
+                }
+            }
+        	
+            if (!grade_enter) {
+                //System.out.println(String.format("Grade of %s don't enter.", course_to_enter));
+            	JOptionPane.showMessageDialog(null, String.format("课程  %s 的成绩未录入.", course_to_enter));
+            }
+            }
+        }
+    }
 }
+
